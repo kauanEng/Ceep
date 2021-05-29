02c2b5f6fa1f1a4f09e6decd8890ecf77f8450fd
Kauan santos <kauansantos83@gmail.com>
2021-05-28 22:46:58 -0300

    finalizado app

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ class App extends Component {
 
 constructor() {
   super()
-  this.notas = [];
+  this.state = {
+  notas:[]
+  }
 }
 
   criarNota(titulo, texto) {
     const novaNota = {titulo, texto};
-    this.notas.push(novaNota);
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+
+    this.setState(novoEstado)
     
   }
 
@@ -20,7 +27,7 @@ constructor() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas notas={this.notas}  />
+        <ListaDeNotas notas={this.state.notas}  />
       </section>
     );
   }
